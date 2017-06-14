e07ea12ccfbd47bdda3f803709eaa220a7ad6029
Peng Jie <bivinity.pengzjie@gmail.com>
2017-06-14 14:04:45 +0800

    Add watch accessToken flow testing

--- sagas/index.js
+++ sagas/index.js
@@ -1,3 +1,17 @@
+import { all, call, take, put, fork } from 'redux-saga/effects';
+import { requestApiToken } from '../api';
+import * as actions from '../actions/auth';
+
+export function* watchLoadAccessToken() {
+  while (true) {
+    const { code } = yield take(actions.REQUEST_ACCESSTOKEN);
+    const { data: { access_token, user } } = yield call(requestApiToken, code);
+    yield put(actions.receiveAccessToken({ access_token, user }));
+  }
+}
+
 export default function* rootSaga() {
-  console.log('root saga');
+  yield all([
+    fork(watchLoadAccessToken)
+  ]);
 }
