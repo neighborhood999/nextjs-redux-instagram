51cd3f5fead95b26f998e5269d4c5bd87ad37e2e
Peng Jie <bivinity.pengzjie@gmail.com>
2017-06-16 10:20:31 +0800

    Add fetchUserProfileAndPhotos Saga

--- sagas/__tests__/index.spec.js
+++ sagas/__tests__/index.spec.js
@@ -1,7 +1,16 @@
-import { call, take, put } from 'redux-saga/effects';
-import { watchLoadAccessToken } from '../';
+import { call, take, put, select } from 'redux-saga/effects';
+import {
+  watchLoadAccessToken,
+  watchFetchUserProfileAndPhotos,
+  fetchInstagramAPIEndpoints
+} from '../';
+import fetchJSONP from 'fetch-jsonp';
+import { endPoint } from '../../utils';
 import { requestApiToken } from '../../api';
+import { selectAuthState } from '../../reducers/selectors';
 import * as authActions from '../../actions/auth';
+import * as userActions from '../../actions/user';
+import { mockUser, mockPhotos } from '../../actions/__tests__/user.spec';
 
 describe('redux saga', () => {
   test('should watch load access token flow', () => {
@@ -29,5 +38,62 @@ describe('redux saga', () => {
     expect(generator.next(mockResponseData).value).toEqual(
       put(authActions.receiveAccessToken({ access_token, user }))
     );
+  });
+
+  test('should watch fetch user profile and photos', () => {
+    const generator = watchFetchUserProfileAndPhotos();
+    const mockAuthState = {
+      accessToken: '66bdb115508aae38640bffb69ffe32a6',
+      user: {
+        id: '999'
+      }
+    };
+
+    expect(generator.next().value).toEqual(
+      take(userActions.REQUEST_USER_AND_PHOTOS)
+    );
+
+    expect(generator.next().value).toEqual(select(selectAuthState));
+
+    expect(generator.next(mockAuthState).value).toEqual(
+      call(
+        fetchInstagramAPIEndpoints,
+        mockAuthState.user['id'],
+        mockAuthState.accessToken
+      )
+    );
+
+    expect(generator.next(mockUser).value).toEqual(
+      put(userActions.receiveUser(mockUser))
+    );
+
+    expect(generator.next().value).toEqual(
+      call(
+        fetchInstagramAPIEndpoints,
+        mockAuthState.user['id'],
+        mockAuthState.accessToken,
+        'media/recent/'
+      )
+    );
+
+    expect(generator.next(mockPhotos).value).toEqual(
+      put(userActions.receiveSelfPhotos(mockPhotos))
+    );
+  });
+
+  test('should create fetchInstagramAPIEndpoints Saga', () => {
+    const mockID = '999';
+    const mockToken = '66bdb115508aae38640bffb69ffe32a6';
+    const mockResponse = {
+      json() {
+        return 'Something Data';
+      }
+    };
+    const generator = fetchInstagramAPIEndpoints(mockID, mockToken);
+
+    expect(generator.next().value).toEqual(
+      call(fetchJSONP, endPoint(mockID, mockToken))
+    );
+    expect(generator.next(mockResponse).value).toEqual(mockResponse.json());
   });
 });
